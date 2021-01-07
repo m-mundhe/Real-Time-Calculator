216e381f417effb19264cf156056c7c53a9054c4
m-mundhe <mmundhe@uncc.edu>
2021-01-07 14:01:41 -0500

    Updated baseurl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { HistoryComponent } from './history/history.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:8000', options: {} }; //For Developemnt
-// const config: SocketIoConfig = { url: window.location.origin, options: {} }; //For Production
+// const config: SocketIoConfig = { url: 'http://localhost:8000', options: {} }; //For Developemnt
+const config: SocketIoConfig = { url: window.location.origin, options: {} }; //For Production
 // const config: SocketIoConfig = { url: "https://tranquil-island-79700.herokuapp.com", options: {} }; //For Production
 
 @NgModule({
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -22,8 +22,8 @@ export class CalculatorComponent implements OnInit {
     this.getCalculations();
   }
 
-  baseURL: string = "http://localhost:8000"; //For Developemnt
-  // baseURL: string = window.location.origin; //For Production
+  // baseURL: string = "http://localhost:8000"; //For Developemnt
+  baseURL: string = window.location.origin; //For Production
   // baseURL: string = "https://tranquil-island-79700.herokuapp.com"; //For Production
   input: string = '';
   result: string = '';
