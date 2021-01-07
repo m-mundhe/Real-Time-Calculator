7bb1340aaec7f4dc4f9e33f966b75324f0734510
m-mundhe <mmundhe@uncc.edu>
2021-01-07 03:47:14 -0500

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+console.log(process.env.MONGO_URL);
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
